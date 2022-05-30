b014ff4e5114e15378698b04c1c4375045431d7d
NzyC <nidal.chowdhury1@gmail.com>
2022-05-30 23:38:54 +1000

    jsssbby

---
+++ WEEK 3/Crash Course/Section 1/index.js
@@ -0,0 +1,11 @@
+// document.getElementById("count-el").innerText = 5
+
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+function call42() {
+    console.log(42)
+}
+
+call42()
