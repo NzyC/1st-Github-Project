bc945484eac6cf21a8639553b2234ca7d12b1fdf
NzyC <nidal.chowdhury1@gmail.com>
2022-06-03 18:53:35 +1000

    addpts

--- WEEK 3/Crash Course/Section 1/Challenges/index.js
+++ WEEK 3/Crash Course/Section 1/Challenges/index.js
@@ -1,8 +1,42 @@
-let welcomeEl = document.getElementById("welcome-el")
+// let welcomeEl = document.getElementById("welcome-el")
 
-let name = "Nidal"
-let greeting = "Welcome Back, "
+// let name = "Nidal"
+// let greeting = "Welcome Back, "
 
-welcomeEl.innerText = greeting + name
+// welcomeEl.innerText = greeting + name
 
-welcomeEl.innerText += " 👋"
+// welcomeEl.innerText += " 👋"
+
+// let firstName = "Nidal"
+// let lastName = "Chowdhury"
+
+// let fullName = firstName + " " + lastName
+
+// console.log(fullName)
+
+// let name = "Linda"
+// let greeting = "Hi there"
+
+// function logGreeting() {
+//     console.log(greeting + ", " + name + "!")
+// }
+
+// logGreeting()
+
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+}
+
+function remove1Point() {
+    myPoints += -1
+}
+
+add3Points()
+remove1Point()
+add3Points()
+add3Points()
+remove1Point()
+
+console.log(myPoints)
