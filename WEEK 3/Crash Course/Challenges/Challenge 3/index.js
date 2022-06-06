1ff92455e05839eb86cf7d95e15b2e988d1ca762
NzyC <nidal.chowdhury1@gmail.com>
2022-06-06 15:10:21 +1000

    blackjack game

--- WEEK 3/Crash Course/Challenges/Challenge 3/index.js
+++ WEEK 3/Crash Course/Challenges/Challenge 3/index.js
@@ -1,26 +1,62 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+// // let num1 = 8
+// // let num2 = 2
+// // document.getElementById("num1-el").textContent = num1
+// // document.getElementById("num2-el").textContent = num2
 
-let sumEl = document.getElementById("sum-el")
+// // let sumEl = document.getElementById("sum-el")
 
-function add() {
-    let sum = num1 + num2
-    sumEl.textContent = " " + sum
-}
+// // function add() {
+// //     let sum = num1 + num2
+// //     sumEl.textContent = " " + sum
+// // }
 
-function subtract() {
-    let sum = num1 - num2
-    sumEl.textContent = " " + sum
-}
+// // function subtract() {
+// //     let sum = num1 - num2
+// //     sumEl.textContent = " " + sum
+// // }
 
-function multiply() {
-    let sum = num1 * num2
-    sumEl.textContent = " " + sum
-}
+// // function multiply() {
+// //     let sum = num1 * num2
+// //     sumEl.textContent = " " + sum
+// // }
 
-function divide() {
-    let sum = num1 / num2
-    sumEl.textContent = " " + sum
-}
+// // function divide() {
+// //     let sum = num1 / num2
+// //     sumEl.textContent = " " + sum
+// // }
+
+// // Arrays
+
+// // let featuredPosts = [
+// //     "Check out my Netflix clone", 
+// //     "Here's the code for my project", 
+// //     "I've just relaunched my Portfolio"
+// // ]
+
+// // console.log( featuredPosts.length )
+
+// // let aboutMe = [
+// //     "Nidal Chowdhury", 
+// //     17, 
+// //     true
+// // ]
+
+// let messages = [
+//     "Hey, how's it going?",
+//     "I'm great, Thank you! How about you?",
+//     "All good, Been working in my portfolio lately."
+// ]
+
+// let newMessage = "Same here!"
+
+// messages.push ( newMessage )
+// messages.pop ()
+
+// console.log(messages)
+
+// for ( let count = 10; count < 21; count += 1 ) {
+
+//     console.log(count)
+
+// }
+
