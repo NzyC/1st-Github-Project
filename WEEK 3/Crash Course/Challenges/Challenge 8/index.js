9e4b22156e2f6241de1a08902cac32b32aecef35
NzyC <nidal.chowdhury1@gmail.com>
2022-06-16 09:17:43 +1000

    lol

---
+++ WEEK 3/Crash Course/Challenges/Challenge 8/index.js
@@ -0,0 +1,12 @@
+const recipient = "James"
+const sender = "Nidal"
+
+const email = `
+    Hey ${recipient}! 
+
+    How's it going? Cheers 
+    
+    ${sender}
+`
+
+console.log(email)
---
+++ WEEK 3/Crash Course/Challenges/Challenge 9/index.js
@@ -0,0 +1,7 @@
+const credits = 12
+
+if (credits) {
+    console.log("Lets play 🎰!")
+} else {
+    console.log("Sorry, you have no credits :(")
+}
