9fd55987bbf1ee25334707e39530ce12b8e37ae8
NzyC <nidal.chowdhury1@gmail.com>
2022-06-16 10:13:00 +1000

    cmonn

---
+++ WEEK 3/Crash Course/Challenges/Challenge 10/index.js
@@ -0,0 +1,13 @@
+// const welcomEL = document.getElementById("welcome-el")
+
+// function greetUser(greeting, name, emoji) {
+//     welcomEL.textContent = `${greeting} , ${name} ${emoji}`
+// }
+
+// greetUser("Welcome back", "Nidal", "👋")
+
+function add(a, b) {
+    
+}
+
+console.log( add(3,4) )
