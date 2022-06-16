253189635e5a0929e3999d13b32184193eaac3ef
NzyC <nidal.chowdhury1@gmail.com>
2022-06-16 10:13:06 +1000

    olo

--- WEEK 3/Crash Course/Lead Finder Chrome Extension/index.js
+++ WEEK 3/Crash Course/Lead Finder Chrome Extension/index.js
@@ -1,34 +1,41 @@
 let myLeads = []
-
+let oldLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
- 
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++ ) {
+        listItems += `
+            <li>
+                <a href='${leads[i]}' target='_blank'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 
     console.log( localStorage.getItem("myLeads"))
 })
-
-function renderLeads() {
-    let listItems = ""
-
-    for (let i = 0; i < myLeads.length; i++ ) {
-     // listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>"
-        listItems += `
-            <li>
-                <a href='${myLeads[i]}' target='_blank'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-
-    }
-
-    ulEl.innerHTML = listItems
-}
