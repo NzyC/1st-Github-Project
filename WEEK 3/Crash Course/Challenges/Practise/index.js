8f6d2e35343cafd7af00ca40f9fdfe4899bdad15
NzyC <nidal.chowdhury1@gmail.com>
2022-06-19 10:24:26 +1000

    thru

--- WEEK 3/Crash Course/Challenges/Practise/index.js
+++ WEEK 3/Crash Course/Challenges/Practise/index.js
@@ -1,32 +1,84 @@
-// const player = "Nidal"
-// const opponent = "Per"
-// const game = "AmazingFighter"
-// let points = 0
-// let hasWon = false
+// // const player = "Nidal"
+// // const opponent = "Per"
+// // const game = "AmazingFighter"
+// // let points = 0
+// // let hasWon = false
 
-// points += 100
-// hasWon = true
+// // points += 100
+// // hasWon = true
 
-// if (hasWon) {
-//     console.log(`${player} got ${points} points and won the ${game} game!`)
-// } else {
-//     console.log("The winner is " + opponent + "!" + player + " lost the game")
-// }
+// // if (hasWon) {
+// //     console.log(`${player} got ${points} points and won the ${game} game!`)
+// // } else {
+// //     console.log("The winner is " + opponent + "!" + player + " lost the game")
+// // }
 
-// let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
+// // let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
 
-// function logCourses(arr) {
-//     for (let i = 0; i < arr.length; i++ ) {
-//         console.log(arr[i])
+// // function logCourses(arr) {
+// //     for (let i = 0; i < arr.length; i++ ) {
+// //         console.log(arr[i])
+// //     }
+// // }
+
+// // logCourses(myCourses)
+
+// // let item = ["lol"]
+
+// // localStorage.setItem("items", JSON.stringify(item))
+
+// // let itemsFromLocalStorage = JSON.parse(localStorage.getItem("items"))
+
+// // console.log(itemsFromLocalStorage)
+
+
+// // let data = [
+// //     {
+// //         player: "Jane",
+// //         score: 52
+// //     },
+// //     {
+// //         player: "Mark",
+// //         score: 41
+// //     }
+// // ]
+
+// // const buttonEl = document.getElementById("button-el")
+
+// // buttonEl.addEventListener("click", function() {
+// //     console.log(data[0].score)
+// // })
+
+// function generateSentence(desc, arr) {
+//     let baseString = `The ${arr.length} ${desc} are `
+//     const lastIndex = arr.length - 1
+//     for (let i = 0; i < arr.length; i++) {
+//         if ( i === lastIndex) {
+//             baseString += arr[i]
+//         } else {
+//             baseString += arr[i] + ", "
+//         }   
 //     }
+//     return baseString
 // }
 
-// logCourses(myCourses)
+// const sentence = generateSentence("largest countries", ["China", "USA", "India"] )
+// console.log(sentence)
 
-let item = ["lol"]
+const imgs = [
+    "assets/hip1.jpg",
+    "assets/hip2.jpg",
+    "assets/hip3.jpg"
+]
 
-localStorage.setItem("items", JSON.stringify(item))
+const container = document.getElementById("container")
 
-let itemsFromLocalStorage = JSON.parse(localStorage.getItem("items"))
+function renderImg() {
+    let imageHolder = ""
+    for (let i = 0; i < imgs.length; i++) {
+        imageHolder += `<img src="${imgs[i]}" class="team-img">`
+    }
+    container.innerHTML = imageHolder
+}
 
-console.log(itemsFromLocalStorage)
+renderImg(imgs)
