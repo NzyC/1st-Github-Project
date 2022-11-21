0571319af5c9d672ce35a4ee95756f897a9acf08
Nzy <nidal.chowdhury1@gmail.com>
2022-11-21 17:59:01 +1100

    upload

--- Adv Projects/Final Project/index.js
+++ Adv Projects/Final Project/index.js
@@ -1,1 +1,25 @@
 // MOVIE API = https://www.omdbapi.com/?i=tt3896198&apikey=5d41d2f2&s=${}
+const filmCardContainerEl = document.querySelector(".film__card--container")
+const searchInputEl = document.getElementById("searchInput")
+
+async function main() {
+  const movies = await fetch(`https:www.omdbapi.com/?i=tt3896198&apikey=5d41d2f2&s=${searchInputEl}`)
+  const moviesData = await movies.json()
+  const moviesNewData = moviesData.Search
+  filmCardContainerEl.innerHTML = moviesNewData.map((e) => filmHTML(e)).slice(0, 6).join('')
+  console.log(moviesData);
+}
+
+main()
+
+function filmHTML(film) {
+  return `
+  <div class="film__card">
+    <img src="${film.Poster}" class="film__poster">
+    <h1 class="film__title">${film.Title}</h1>
+    <p class="film__release">Year: ${film.Year}</p>
+    <p class="film__type">Type: ${film.Type}</p>
+    <p class="film__type">IMDB ID: ${film.imdbID}</p>
+  </div>
+  `
+} 
