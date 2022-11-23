96dd46ee1431bab471087e8cb7d3da28068e1450
Nzy <nidal.chowdhury1@gmail.com>
2022-11-23 21:56:17 +1100

    upload

--- Adv Projects/Final Project/index.js
+++ Adv Projects/Final Project/index.js
@@ -1,13 +1,36 @@
 // MOVIE API = https://www.omdbapi.com/?i=tt3896198&apikey=5d41d2f2&s=${}
 const filmCardContainerEl = document.querySelector(".film__card--container")
-const searchInputValue = document.getElementById("searchInput").value
+const searchInput = document.getElementById("searchInput")
+const searchBtnEl = document.querySelector('.fa-solid')
+const loadingEl = document.querySelector('.loading')
+const initLandingEl = document.querySelector('.initial__landing')
+const topLandingEl = document.querySelector('.top__landing')
 
 async function main() {
-  const movies = await fetch(`https:www.omdbapi.com/?i=tt3896198&apikey=5d41d2f2&s=harry`)
+  searchBtnEl.addEventListener('click', a => {
+    renderMovies()
+  })
+  searchInput.addEventListener('keypress', e => {
+    if (e.key === 'Enter') {
+
+      renderMovies()
+    }
+  })
+}
+
+function lol() {
+  console.log('hi')
+}
+
+async function renderMovies() {
+  loadingEl.classList.remove('dontShow')
+  initLandingEl.classList += ' dontShow'
+  topLandingEl.classList += ' dontShow'
+  const movies = await fetch(`https:www.omdbapi.com/?i=tt3896198&apikey=5d41d2f2&s=${searchInput.value}`)
   const moviesData = await movies.json()
-  const moviesNewData = moviesData.Search
-  filmCardContainerEl.innerHTML = moviesNewData.map((e) => filmHTML(e)).slice(0, 6).join('')
-  console.log(moviesData);
+  loadingEl.classList += ' dontShow'
+  topLandingEl.classList.remove('dontShow')
+  filmCardContainerEl.innerHTML = moviesData.Search.map((e) => filmHTML(e)).slice(0, 6).join('')
 }
 
 main()
